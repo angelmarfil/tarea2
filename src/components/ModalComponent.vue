e5851e349972c2f543aa28f808a195bfb2847af3
Angel Marfil <angelmarfil11@gmail.com>
2024-02-15 11:25:39 -0500

    feat: Refactor validations to use computed values

--- src/components/ModalComponent.vue
+++ src/components/ModalComponent.vue
@@ -1,51 +1,117 @@
+<script setup lang="ts">
+import gamesData from '@/data/gamesData'
+import type { IGame } from '@/interfaces/IGame'
+import type { Ref, ComputedRef } from 'vue'
+import { ref, computed } from 'vue'
+
+const dialog = ref<HTMLDialogElement>()
+const games = ref(gamesData)
+
+const image: Ref<string> = ref('')
+const name: Ref<string> = ref('')
+const price: Ref = ref(null)
+const date: Ref<string> = ref('')
+const theme: Ref<string> = ref('default')
+
+const imageValidation = computed(() =>
+  image.value.length > 0 && !URL.canParse(image.value) ? 'La URL debe ser válida.' : ''
+)
+
+const nameValidation = computed(() => {
+  if ((name.value.length > 0 && name.value.length < 4) || name.value.length > 60) {
+    return 'Error de longitud en el nombre (mínimo 4, máximo 60 caracteres).'
+  }
+  if (isNameDuplicate(name.value)) {
+    return 'Este nombre ya existe en la lista'
+  }
+
+  return ''
+})
+
+const priceValidation = computed(() =>
+  price.value < 0 || price.value > 10000 ? 'El precio debe ser entre 0 y 10000.' : ''
+)
+
+const isFormValid: ComputedRef<boolean> = computed(
+  () =>
+    !(
+      imageValidation.value.length > 0 ||
+      nameValidation.value.length > 0 ||
+      priceValidation.value.length > 0
+    )
+)
+
+// formateo de fecha máxima (Al día de hoy)
+const maxDate = ref<string>('')
+const today = new Date()
+const year = today.getFullYear()
+const month = (today.getMonth() + 1).toString().padStart(2, '0')
+const day = today.getDate().toString().padStart(2, '0')
+maxDate.value = `${year}-${month}-${day}`
+
+// funciones para el modal
+const showModal = () => {
+  dialog.value?.showModal()
+}
+
+const closeModal = () => {
+  dialog.value?.close()
+}
+
+defineExpose({
+  show: showModal
+})
+
+const handleSubmit = () => {
+  if (isFormValid.value) {
+    const newGame: IGame = {
+      id: gamesData.length + 1,
+      name: name.value,
+      image: image.value,
+      price: price.value,
+      theme: theme.value,
+      release_date: new Date(date.value)
+    }
+
+    games.value.push(newGame)
+    closeModal()
+    alert('El juego ha sido agregado a la lista.')
+
+    image.value = ''
+    name.value = ''
+    price.value = null
+    ;(date.value = ''), (theme.value = 'default')
+  }
+}
+
+const isNameDuplicate = (value: string): boolean => {
+  return games.value.some((game: IGame) => game.name.toLowerCase() === value.toLowerCase())
+}
+</script>
+
 <template>
   <button class="add" @click="showModal">Agregar Juego</button>
   <dialog ref="dialog">
     <h2>Agregar Juego</h2>
     <form @submit.prevent="handleSubmit">
-      <input
-        @input="validation('image')"
-        v-model.trim="image"
-        type="url"
-        placeholder="URL de imagen"
-        name="image"
-        required
-      />
-      <span v-if="errors.image" class="error">{{ errors.image }}</span>
+      <div class="input_box">
+        <input v-model.trim="image" type="url" placeholder="URL de imagen" name="image" required />
+        <span v-if="imageValidation" class="error">{{ imageValidation }}</span>
+      </div>
 
-      <input
-        @input="validation('name')"
-        v-model.trim="name"
-        type="text"
-        placeholder="Nombre"
-        name="name"
-        required
-      />
-      <span v-if="errors.name" class="error">{{ errors.name }}</span>
+      <div class="input_box">
+        <input v-model.trim="name" type="text" placeholder="Nombre" name="name" required />
+        <span v-if="nameValidation" class="error">{{ nameValidation }}</span>
+      </div>
+
       <div>
-        <div class="small">
-          <input
-            @input="validation('price')"
-            v-model="price"
-            type="number"
-            placeholder="Precio"
-            name="price"
-            required
-          />
-          <span v-if="errors.price" class="error">{{ errors.price }}</span>
+        <div class="small input_box">
+          <input v-model="price" type="number" placeholder="Precio" name="price" required />
+          <span v-if="priceValidation" class="error">{{ priceValidation }}</span>
         </div>
 
         <div class="small">
-          <input
-            @input="validation('date')"
-            v-model="date"
-            type="date"
-            min="2017-03-03"
-            :max="maxDate"
-            name="date"
-            required
-          />
-          <span v-if="errors.date" class="error">{{ errors.date }}</span>
+          <input v-model="date" type="date" min="2017-03-03" :max="maxDate" name="date" required />
         </div>
         <div class="small">
           <select v-model="theme" name="theme">
@@ -64,111 +130,6 @@
     </form>
   </dialog>
 </template>
-<script setup lang="ts">
-import gamesData from '@/data/gamesData'
-import type { IGame } from '@/interfaces/IGame'
-import { ref, nextTick, type Ref, onMounted } from 'vue'
-
-const dialog = ref<HTMLDialogElement>()
-const games = ref(gamesData)
-
-const image: Ref<string> = ref('')
-const name: Ref<string> = ref('')
-const price: Ref = ref(null)
-const date: Ref<string> = ref('')
-const theme: Ref<string> = ref('default')
-
-const maxDate = ref<string>('')
-
-onMounted(() => {
-  const today = new Date()
-  const year = today.getFullYear()
-  const month = (today.getMonth() + 1).toString().padStart(2, '0')
-  const day = today.getDate().toString().padStart(2, '0')
-  maxDate.value = `${year}-${month}-${day}`
-})
-
-const errors: Ref<Record<string, string>> = ref({})
-const isFormValid = ref(true)
-
-const showModal = () => {
-  dialog.value?.showModal()
-}
-
-const closeModal = () => {
-  dialog.value?.close()
-}
-
-defineExpose({
-  show: showModal
-})
-
-const validation = (field: string) => {
-  errors.value = { ...errors.value, [field]: '' }
-
-  switch (field) {
-    case 'image':
-      if (!URL.canParse(image.value)) {
-        errors.value[field] = 'La URL debe ser válida.'
-      }
-      break
-    case 'name':
-      if (name.value.length < 4 || name.value.length > 60) {
-        errors.value[field] = 'Error de longitud en el nombre (mínimo 4, máximo 60 caracteres).'
-      }
-      else if(isNameDuplicate(name.value)){
-        errors.value[field] = 'Este nombre ya existe en la lista.'
-      }
-      break
-    case 'price':
-      if (price.value < 0 || price.value > 10000) {
-        errors.value[field] = 'El precio debe ser entre 0 y 10000.'
-      }
-      break
-  }
-
-  validateForm()
-}
-
-const validateForm = () => {
-  nextTick(() => {
-    isFormValid.value = Object.values(errors.value).every((error) => error === '')
-  })
-}
-
-const handleSubmit = () => {
-  if (isFormValid.value) {
-    const newGame: IGame = {
-      id: gamesData.length + 1,
-      name: name.value,
-      image: image.value,
-      price: price.value,
-      theme: theme.value,
-      release_date: new Date(date.value)
-    }
-
-    games.value.push(newGame)
-    closeModal()
-    alert('El juego ha sido agregado a la lista.')
-    resetForm()
-  }
-}
-
-const resetForm = () => {
-  image.value = ''
-  name.value = ''
-  price.value = null
-  date.value = '',
-  theme.value = '',
-  errors.value = {}
-  isFormValid.value = true
-}
-
-const isNameDuplicate = (value: string): boolean => {
-  return games.value.some((game: IGame) => game.name.toLowerCase() === value.toLowerCase())
-}
-
-</script>
 
 <style scoped>
 dialog {
@@ -186,16 +147,15 @@ dialog {
     backdrop-filter: blur(4px);
   }
 
-  @media(width < 1260px){
+  @media (width < 1260px) {
     & {
-        width: 70vw;
+      width: 70vw;
     }
 
     select {
-        width: 200px;
+      width: 200px;
     }
   }
-
 }
 
 .add {
@@ -240,12 +200,21 @@ button {
 }
 
 select {
-    position: relative;
-    padding: 10px 20px;
-    width: 300px;
-    font-family: 'Poppins';
-    border-radius: 8px;
-    border: 1px solid grey;
+  position: relative;
+  padding: 10px 20px;
+  width: 300px;
+  font-family: 'Poppins';
+  border-radius: 8px;
+  border: 1px solid grey;
+}
+
+.input_box {
+  display: flex;
+  flex-direction: column;
+  row-gap: 0.5rem;
+  & span {
+    padding-left: 0.4rem;
+  }
 }
 
 input {
